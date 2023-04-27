2ffafa200c58899dca1bf2a134bace7b8046e0b8
Yeon-Jun-Kim <kk2522@naver.com>
2023-04-27 14:29:12 +0900

    mail 내용 변경

--- src/main/java/com/mail/Mail.java
+++ src/main/java/com/mail/Mail.java
@@ -1,5 +1,15 @@
 package com.mail;
 
+import lombok.*;
+
+@Getter @Setter
+@NoArgsConstructor
 public class Mail implements MailVisual {
+
+	public void view() {//파라미터로 db에 저장된 거 가져오기
+		System.out.println(BANNER[0]);//인덱스 : db에 저장된 값 가져오기
+		System.out.println(BACKGROUDCOLOR[1]+TEXTCOLOR[4]+ "DB.CONTENT");//인덱스 : db에 저장된 값 가져오기
+		System.out.println();
+	}
 	 
 }
