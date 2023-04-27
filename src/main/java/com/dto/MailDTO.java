ea2c538359849797e0f2bab64d6b8eb272962926
syunwi2 <syunwi2@gmail.com>
2023-04-27 10:24:57 +0900

    Update DTOs

--- src/main/java/com/dto/MailDTO.java
+++ src/main/java/com/dto/MailDTO.java
@@ -1,5 +1,20 @@
 package com.dto;
 
-public class MailDTO {
+import java.util.Calendar;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class MailDTO {
+	int mail_no;
+	String mail_title;
+	String mail_content;
+	int mail_anonymity;
+	Calendar mail_date;
+	int send_user_no;
+	int receive_user_no;
 }
