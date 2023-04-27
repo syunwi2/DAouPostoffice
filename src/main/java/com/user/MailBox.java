4f5701bae5f5e686156d7dcd67df0299eda9aa1d
Yeon Jun <107024779+Yeon-Jun-Kim@users.noreply.github.com>
2023-04-27 10:28:36 +0900

    1

--- src/main/java/com/user/MailBox.java
+++ src/main/java/com/user/MailBox.java
@@ -1,5 +1,14 @@
 package com.user;
 
-public interface MailBox {
+import lombok.Data;
 
+@Data
+
+public interface MailBox {
+	
+	public String[] textColor = new String[10];
+	public String[] shape = new String[10];
+	public abstract void viewBox();
+	public abstract void upDateBox();
+	
 }
