e9b4217656088e764c451c2f5ed2f1ccebae4e02
unknown <rdg9074@gmail.com>
2023-04-27 17:46:55 +0900

    login

--- src/main/java/com/user/UserHandler.java
+++ src/main/java/com/user/UserHandler.java
@@ -10,18 +10,14 @@ import com.service.ServiceImpl;
 
 public class UserHandler {
 	
-	
-	Scanner scan = new Scanner(System.in);
-	List<UserDTO> list = new ArrayList<UserDTO>();
-	
-	String inputId, inputPasswd;
 	User user = new User();
 	UserDTO dto = new UserDTO();
-	
+	Scanner scan = new Scanner(System.in);
+
 	//run
 	public void run() {
 		System.out.println("choice pz");
-		System.out.println("1: sign === 2: login === 3: member check ");		
+		System.out.println("1: sign === 2: login === ");		
 		int ch=0;
 		switch (ch=scan.nextInt()) {
 			case 1:
@@ -30,16 +26,9 @@ public class UserHandler {
 			case 2:
 				login();
 				break;
-			case 3:
-				memch();
-				break;
 		}
-		
 	}
 	
-	public void memberPut() {
-		
-	}
 	
 	//sign up
 	//user에서 유저 클래스를 받아와서 객체 만들고 이 형식에 맞게 저장.
@@ -78,7 +67,7 @@ public class UserHandler {
 			
 			System.out.println(" 이름을 입력하시오. ");
 			String namePut = scan.next();
-			System.out.println(idPut+"\t"+pwPut+"\t"+namePut);
+			System.out.println(idPut+"\t"+namePut);
 			
 			UserDTO dto = new UserDTO(0, idPut, pwPut, namePut);
 			Service service = new ServiceImpl();
@@ -94,19 +83,22 @@ public class UserHandler {
 	public void login() {
 		System.out.println(" login");
 		System.out.println(" ======================== ");
+		
 		System.out.print("아이디를 입력하세요 : ");
-        inputId = scan.next();
+        dto.setUser_id(scan.next());
+        
         System.out.print("비밀번호를 입력하세요 : ");
-        inputPasswd = scan.next();
+        dto.setUser_passwd(scan.next());
+        
+        Service service = new ServiceImpl();
+        dto = service.findUser(dto);
+        System.out.println(dto);
+        if (dto != null)
+        	System.out.println("login success.");
         
 	}
 	//logout
 	public void logout() {
-		
-	}
-	
-	//memch
-	public void memch() {
 		
 	}
 }
